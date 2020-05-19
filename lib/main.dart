8167ca1d38d5119b9c7fe6fb4d34a8b56b68c3fd
TeddyJubu <syeedmdjobayer@gmail.com>
2020-05-19 16:34:10 +0600

    image added in the container and centered

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(Magic8BallApp());
+
+class Magic8BallApp extends StatefulWidget {
+  @override
+  _Magic8BallAppState createState() => _Magic8BallAppState();
+}
+
+class _Magic8BallAppState extends State<Magic8BallApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(
+            child: Text(
+              'Ask Me Anything',
+            ),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            child: FlatButton(
+              onPressed: () {
+                print('pressed');
+              },
+              child: Image.asset('images/ball1.png'),
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
